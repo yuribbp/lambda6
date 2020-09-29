9eba8ff9644649fdb99a20aa73033d54646cb487
Yuri Baldan <yuribbp@gmail.com>
2020-09-29 18:38:01 -0300

    Final implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -32,6 +32,21 @@ public class Program {
 				list.add(new Product(fields[0], Double.parseDouble(fields[1])));
 				line = br.readLine();
 			}
+			
+			double avg = list.stream()
+					.map(p -> p.getPrice())
+					.reduce(0.0, (x,y) -> x + y) / list.size();
+			
+			System.out.println("Average price: " + String.format("%.2f", avg));
+			
+			Comparator<String> comp = (s1, s2) -> s1.toUpperCase().compareTo(s2.toUpperCase());
+			
+			List<String> names = list.stream()
+					.filter(p -> p.getPrice() < avg)
+					.map(p -> p.getName()).sorted(comp.reversed())
+					.collect(Collectors.toList());
+			
+			names.forEach(System.out::println);
 		}
 		catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
